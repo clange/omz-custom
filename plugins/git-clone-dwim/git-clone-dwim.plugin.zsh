19030ea5086e4eeaa2818710119df078c790343c
Christoph Lange <christoph.lange-bever@fit.fraunhofer.de>
2023-08-18 09:42:09 +0200

    allowing further domains

--- plugins/git-clone-dwim/git-clone-dwim.plugin.zsh
+++ plugins/git-clone-dwim/git-clone-dwim.plugin.zsh
@@ -1,8 +1,10 @@
 function git-clone-dwim()
 {
+    setopt local_options re_match_pcre
     url="$1"
-    if [[ $url =~ https://(github\.com|gitlab\.com)/(.+)/([^/]+) ]]
+    if [[ $url =~ https://(github\.com|gitlab\.com|(?:git(?:lab)?\.)[^.]+\.[^.]+)/(.+)/([^/]+) ]]
     then
+        # TODO strip git. or gitlab. subdomains
         domain="${match[1]}"
         organization="${match[2]}"
         repository="${match[3]}"
